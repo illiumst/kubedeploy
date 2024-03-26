19bc3bd54181d39b066fa7fa5d25b565d0c12432
Steffen Illium <steffen.illium@gmail.com>
2024-03-26 12:22:34 +0100

    metallb and argocd rdy

--- infrastructure/01-default-setup.sh
+++ infrastructure/01-default-setup.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 # Install MetalLB and configure ip ranges (local homelab)
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.3/config/manifests/metallb-native.yaml
-              -f ./01-metallb-pool.yml
-              -f ./01-metallb-advert.yml
+kubectl apply -k ./01-metallb
 
 # Install argoCD for deployment and secret management
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
+kustomize apply -f ./02-argocd
