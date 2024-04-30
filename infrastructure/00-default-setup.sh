40f0beaca3921394d84b3f0e959e36befe8acecb
Steffen Illium <steffen.illium@gmail.com>
2024-04-30 09:21:24 +0200

    add image updater

--- infrastructure/00-default-setup.sh
+++ infrastructure/00-default-setup.sh
@@ -5,3 +5,4 @@ kubectl apply -k ./01-metallb
 
 # Install argoCD for deployment and secret management
 kubectl apply -k ./02-argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
