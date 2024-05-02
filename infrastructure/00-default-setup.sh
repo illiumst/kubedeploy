00399a72b5d822c8c92c75027eec301c5dbf2148
Steffen Illium <steffen.illium@gmail.com>
2024-05-02 22:54:08 +0200

    smaller adjustments

--- infrastructure/00-default-setup.sh
+++ infrastructure/00-default-setup.sh
@@ -6,3 +6,12 @@ kubectl apply -k ./01-metallb
 # Install argoCD for deployment and secret management
 kubectl apply -k ./02-argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
+
+# Start NFD - if your cluster doesn't have NFD installed yet
+kubectl apply -k 'https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/nfd?ref=v0.29.0'
+
+# Create NodeFeatureRules for detecting GPUs on nodes
+kubectl apply -k 'https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/nfd/overlays/node-feature-rules?ref=v0.29.0'
+
+# Create GPU plugin daemonset
+kubectl apply -k 'https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/gpu_plugin/overlays/nfd_labeled_nodes?ref=v0.29.0'
